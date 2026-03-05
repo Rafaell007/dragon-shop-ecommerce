375570d902f13c42c15988108e19bf34e3a25db0
Rafaell007 <drax1995@tlen.pl>
2026-03-06 00:13:35 +0100

    Add ProductsGrid component to HomePage; implement product display with mock data and quantity selection

--- OKINAWA-ECOMMERCE/src/components/pages/home/HomePage.jsx
+++ OKINAWA-ECOMMERCE/src/components/pages/home/HomePage.jsx
@@ -1,11 +1,13 @@
 import { Header } from "../../header/Header.jsx";
 import { Hero } from "../../hero/Hero.jsx";
+import { ProductsGrid } from "./ProductsGrid.jsx";
 
 export function HomePage() {
     return (
         <>
         <Header />
         <Hero />
+        <ProductsGrid />
         </>
     )
 }
--- OKINAWA-ECOMMERCE/src/components/pages/home/ProductsGrid.jsx
+++ OKINAWA-ECOMMERCE/src/components/pages/home/ProductsGrid.jsx
@@ -1,13 +1,46 @@
+import { mockProducts } from "../../../data/mockProducts";
+import "./ProductsGrid.css";
 
 
 
 
 
-
-return function ProductsGrid() {
+export function ProductsGrid() {
     return(
         <>
-        
+        <div className="products-grid-container">
+            {mockProducts.map((product)=>{
+                return(
+                    <div className="product-container">
+                        <div className="product-image-container">
+                            <img src={product.image} alt={product.name} />
+                        </div>
+                        <div className="product-name-container">
+                            <p>{product.name}</p>
+                        </div>
+                        <div className="product-price-container">
+                            <p>${product.priceCents / 100}</p>
+                        </div>
+                        <div className="product-quantity-container">
+                            <select name="" id="">
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                                <option value="9">9</option>
+                                <option value="10">10</option>
+                            </select>
+                        </div>
+                        <button className="add-to-cart-button">Add to Cart</button>
+                    </div>
+                )
+            })}
+
+        </div>
         </>
     )
 }
